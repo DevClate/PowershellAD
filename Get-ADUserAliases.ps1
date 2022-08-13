88469043b109c602725eee651bc6326f2f8b576a
DevClate <107187714+DevClate@users.noreply.github.com>
2022-08-13 05:38:42 -0400

    Added Parameters

--- Get-ADUserAliases.ps1
+++ Get-ADUserAliases.ps1
@@ -1,4 +1,17 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory,HelpMessage='User to view Aliases')]
+    [string]$Identity,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNull()]
+    [System.Management.Automation.PSCredential]
+    [System.Management.Automation.Credential()]
+    $Credential = [System.Management.Automation.PSCredential]::Empty
+
+)
+
 #Gets multiple aliases for 1 AD user
-Get-ADUser -Identity username -Properties proxyaddresses | Select-Object Name, @{L = "ProxyAddresses"; E = { ($_.ProxyAddresses -like 'smtp:*') -join ";"}} | ConvertTo-HTML | Out-File alias.htm
+Get-ADUser -Identity $Identity -Credential $Credential -Properties proxyaddresses | Select-Object Name, @{L = "ProxyAddresses"; E = { ($_.ProxyAddresses -like 'smtp:*') -join ";"}} | ConvertTo-HTML | Out-File alias.htm
 #Then use Invoke Item to view
 Invoke-Item alias.htm
