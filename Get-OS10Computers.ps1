9641ec5f79ac7dacc68af5fd6dbb4cb0af27675d
DevClate <107187714+DevClate@users.noreply.github.com>
2022-08-13 05:38:53 -0400

    Cleaned up

--- Get-OS10Computers.ps1
+++ Get-OS10Computers.ps1
@@ -1,6 +1,5 @@
 #Exports and stays within powershell
-Get-ADComputer -filter {operatingsystem -like "Windows 10*" } -Properties OperatingSystemVersion|select name,OperatingSystemVersion |sort OperatingSystemVersion -Descending
-
+Get-ADComputer -filter {operatingsystem -like "Windows 10*" } -Properties OperatingSystemVersion|Select-Object name,OperatingSystemVersion |Sort-Object OperatingSystemVersion -Descending
 
 #Use this one to export to CSV
 #Get-ADComputer -filter {operatingsystem -like "Windows 10*" } -Properties lastlogondate,OperatingSystemVersion|select name,OperatingSystemVersion,lastlogondate |sort OperatingSystemVersion -Descending | Export-Csv -Path .\Windows10.csv -NoTypeInformation
