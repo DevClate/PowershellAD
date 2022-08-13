6d4e01a33f454fe8515d979cfaebb1b28fe9a6c3
DevClate <107187714+DevClate@users.noreply.github.com>
2022-08-13 05:38:26 -0400

    Renamed

--- Add-ADSecurityGroup.ps1
+++ Add-ADSecurityGroup.ps1
@@ -1,4 +1,24 @@
 #Create Security Group
 #Created 5/3/2022
-$Credentials = Get-Credential
-New-ADGroup -Name "Security Group Name" -Credential $Credentials -SamAccountName SecurityGroupName -GroupCategory Security -GroupScope Global -DisplayName "Security Group Name" -Path "OU=Security Groups,DC=domain,DC=local" -Description "Description of Security Group" -Confirm
+#Change the OU path if neccesary
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory,HelpMessage='Name of Security Group')]
+    [string]$Name,
+
+    [Parameter(Mandatory,HelpMessage='Display Name of Security Group')]
+    [string]$DisplayName,
+
+    [Parameter(HelpMessage='Description of Security Group')]
+    [string]$Description,
+
+    [Parameter(Mandatory)]
+    [ValidateNotNull()]
+    [System.Management.Automation.PSCredential]
+    [System.Management.Automation.Credential()]
+    $Credential = [System.Management.Automation.PSCredential]::Empty
+
+)
+
+New-ADGroup -Name $Name -Credential $Credentials -SamAccountName $Name -GroupCategory Security -GroupScope Global -DisplayName $DisplayName -Path "OU=Security Groups,DC=domain,DC=local" -Description $Description -Confirm
